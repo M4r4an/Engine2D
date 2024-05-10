a0a6ba0f424631f35daade010455446bc31db1e2
M4r4an <160077604+M4r4an@users.noreply.github.com>
2024-05-10 18:03:45 +0100

    Basic project setup

---
+++ Build.lua
@@ -0,0 +1,17 @@
+-- premake5.lua
+workspace "Engine2D"
+   architecture "x64"
+   configurations { "Debug", "Release", "Dist" }
+   startproject "Sandbox"
+
+   -- Workspace-wide build options for MSVC
+   filter "system:windows"
+      buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus" }
+
+OutputDir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
+
+
+include "Engine2D/Build-Engine.lua"
+
+
+include "Sandbox/Build-App.lua"
